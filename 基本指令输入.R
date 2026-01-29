6c7b2b71d63f27252ab4d3028c033ab6b9abeea8
wtianyuuu <wtianyu@gmail.com>
2026-01-29 12:12:08 +0800

    Update Heatmap.R

---
+++ 基本指令输入.R
@@ -0,0 +1,18 @@
+# ---- packages ----
+library(ggplot2)
+library(dplyr)
+library(tibble)
+library(ggrepel)
+library(scales)
+
+需要在github里面更新脚本，就输入这个指令：绿色的字是解释作用
+git add Heatmap.R
+git commit -m "Update heatmap"
+git push
+
+
+如果是加入文件，输入
+git add Volcano.R
+git commit -m "Add volcano plot script"
+git push
+
